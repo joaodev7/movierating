42229f183e8d2bc39f3db5d9ff603d922d3ab322
João Vitor <82349100+joaodev7@users.noreply.github.com>
2021-12-28 15:42:21 -0300

    Requisição de dados encadeados montada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Tmdb from './Tmdb'
 
 export default () => {
+
+  const [movielist, setMovieList ] = useState([]);
+
+  useEffect(()=>{
+    const loadAll =async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
     <div>
       Olá Mundo!!!
